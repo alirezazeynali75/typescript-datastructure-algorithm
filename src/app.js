f9b466fa96560a42fb4ff708865af4be65f2d4aa
sarahajati <hajatis1227@gmail.com>
2022-11-03 22:03:19 +0330

    first commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var typescript_1 = require("typescript");
+typescript_1.convertCompilerOptionsFromJson;
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { convertCompilerOptionsFromJson } from "typescript";
+
+convertCompilerOptionsFromJson;
